0259ef4cc257bee142fd148ad81cca11ce037d67
Ryan Moore <talkingstove@gmail.com>
2021-03-14 17:34:53 -0700

    filter

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-      <h1>
-        Welcome to the Moore Store!
-      </h1>
       <div v-if="!storeItems.length">
         Loading...
       </div>
       <div v-if="storeItems.length">
-        <ul>
-          <li v-for="item in currentPageItems" :key="item.id">
+        <div>
+          <h4>
+            Category filter
+          </h4>
+          <select @change="selectCategory">
+            <option value="">All</option>
+            <option v-for="cat in categories" :key="cat" :value="cat">
+              {{cat}}
+            </option>
+          </select>
+        </div>
+        <ul key="list">
+          <li v-for="item in currentPageItems" :key="item.id" class="item">
             <StoreItem :item="item" />
           </li>
         </ul>
@@ -39,18 +47,28 @@
 
       //"quick and dirty" pagination on client only in interest of time rm
       axios.get('http://localhost:4000/endpoints/products.json').then(function(res) {
-        console.log(res.data)
         self.storeItems = res.data;
+        self.storeItemsCache = res.data;
         self.maxPage = Math.floor(res.data.length/50);
         self.setCurrentItems();
+
+        for (let i=0; i<self.storeItems.length; i++) {
+          if (self.categories.indexOf(self.storeItems[i].category) === -1) {
+            self.categories.push(self.storeItems[i].category);
+          }
+        }
       })
+
+      
     },
     data: function () {
       return {
          storeItems: [],
+         storeItemsCache: [],
          currentPageItems: [],
          pagination: 0,
-         maxPage: 0
+         maxPage: 0,
+         categories: []
       }
     },
     components: {
@@ -62,6 +80,7 @@
         const startIndex = this.pagination * 50;
         const endIndex = startIndex + 50;
         this.currentPageItems = storeItemsClone.slice(startIndex, endIndex);
+        this.$forceUpdate();
       },
       prevPage: function() {
         if (this.pagination > 0) {
@@ -74,6 +93,29 @@
           this.pagination++;
           this.setCurrentItems();
         }
+      },
+      selectCategory: function(e) {
+        const newFilterVal = e.currentTarget.value;
+
+        if (newFilterVal) {
+          this.filterItems(newFilterVal)
+        }
+        else {
+          this.showAll()
+        }
+      },
+      filterItems: function(filterVal) {
+        this.storeItems = this.storeItemsCache.filter(item => 
+          item.category === filterVal
+        );
+
+        this.pagination = 0; //reset
+        this.setCurrentItems();
+      },
+      showAll: function() {
+        this.storeItems = this.storeItemsCache;
+        this.pagination = 0; //reset
+        this.setCurrentItems();
       }
     }
   }
@@ -99,5 +141,11 @@
 <!-- Scoped component css -->
 <!-- It only affect current component -->
 <style scoped>
-  
+  .item {
+    display: inline-block;
+    margin: 10px;
+    width: 200px;
+    height: 300px;
+    list-style-type: none;
+  }
 </style>
--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-      {{item.name}}
+  <div class="item-wrapper">
+      <img :src="getImgUrl" />
+      <div>
+        {{item.name}}
+      </div>
+      <div>
+        from {{item.manufacturer}}
+      </div>
+      <div>
+         {{getProductType}}
+      </div>
+      <div>
+         {{getPriceFormatted}}
+      </div>
   </div>
 </template>
 
@@ -10,9 +22,20 @@
   export default {
     name: 'store',
     props: ['item'],
+    computed: {
+      getImgUrl: function() {
+        return 'https://res.cloudinary.com/amusenow/w_auto,c_scale/' + this.item.image_url
+      },
+      getProductType: function() {
+        return this.item.category + ", " + this.item.sub_category
+      },
+      getPriceFormatted: function() {
+        return '$' + this.item.price
+      }
+    },
     data: function () {
       return {
-         
+        
       }
     },
     components: {
@@ -28,5 +51,12 @@
 <!-- Scoped component css -->
 <!-- It only affect current component -->
 <style scoped>
-  
+  .item-wrapper {
+    display: flex;
+    flex-direction: column;
+    padding: 5px;
+    border: 1px solid green;
+    font-size: 12px;
+    height: 300px;
+  }
 </style>
