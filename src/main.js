7b91920a04a83b27e2120308d475a9b30c7c2112
jremi <jzbgdev@gmail.com>
2021-03-19 01:56:18 -0700

    main.js (entry file) for Vue instance creation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import './plugins/vue-meta';
+import './plugins/vue-notification';
+import { EventBus } from "@/event-bus.js";
 
 Vue.config.productionTip = false
 
+Vue.prototype.$eventBus = EventBus;
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
