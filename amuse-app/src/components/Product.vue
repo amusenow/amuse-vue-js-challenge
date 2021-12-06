a43eeb7f78d161642e007e74836a0240ab283ff6
Michael Arrington <31665551+arringtonm@users.noreply.github.com>
2021-12-05 16:45:24 -0800

    Import products; build first proof of concept product card

---
+++ amuse-app/src/components/Product.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <h2>{{ name }}</h2>
+      <p>${{ price }}</p>
+  </div>
+</template>
+
+<script>
+export default ({
+  props: [
+    "entity_id",
+    "name",
+    "is_cannabis",
+    "manufacturer",
+    "url",
+    "category",
+    "sub_category",
+    "image_url",
+    "price",
+    "thc_percentage"
+  ],
+})
+</script>
+
+<style scoped lang="scss">
+  .card {
+    padding: 2rem;
+    box-shadow: 0px 3px 6px #dddddd;
+    margin: 1rem;
+    text-align: center;
+  }
+</style>
