5fbebe59f6526f64c00c8fbd9250ded7a22a1d4a
Michael Arrington <31665551+arringtonm@users.noreply.github.com>
2021-12-05 17:57:38 -0800

    Scroll event not firing as expected / dinner break

--- amuse-app/src/components/HelloWorld.vue
+++ amuse-app/src/components/HelloWorld.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
+  <div v-on:scroll="this.shoutAtMe">
     <h1>:)</h1>
-    <div class="product-grid">
-      <product v-for="product in products" 
+    <div id="product-grid" >
+      <product v-for="product in productList" 
         :key="product.entity_id" 
         :name="product.name" 
         :category="product.category"
         :price="product.price"
+        :image_url="product.image_url"
       >
       </product>
     </div>
@@ -24,16 +25,82 @@ export default {
   },
   data() {
     return {
-      products: json
+      products: json,
+      productList: [],
+      productCount: 0,
     }
+  },
+  mounted () {
+    const productGrid = document.querySelector('#product-grid');
+    // eslint-disable-next-line
+    productGrid.addEventListener('scroll', () => {
+      console.log(productGrid.scrollTop);
+      if (productGrid.scrollTop + productGrid.clientHeight >= productGrid.scrollHeight) {
+        console.log('scrolling');
+        this.loadMoreProducts();
+      }
+    });
+    this.loadMoreProducts();
+  },
+  methods: {
+    loadMoreProducts() {
+      console.log('loading!');
+      for (let i = 0; i < 30; i++) {
+        this.productList.push(this.products[i + this.productCount]);
+      }
+    },
+    shoutAtMe: function() {
+      console.log('yayaoaoa');
+    }
+  },
+  computed: {
+    // infiniteScroll: function() {
+    // },
+
+
+    // proof of concept filter
+    filteredProducts: function() {
+      return this.products.filter(product => 
+        product.category === "Flower" ||
+        product.category === "Extract"
+      );
+    },
+
+    // sortByPrice: function(a, b) {
+    //   if (a.price < b.price) { return -1 }
+    //   else if (a.price > b.price) { return 1 }
+    //   return 0;
+    // }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  .product-grid {
-    display: flex;
-    flex-direction: column;
-  }
+  #product-grid {
+    display: grid;
+    grid-gap: 2rem;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    padding: 0.5rem;
+
+    @media only screen and (min-width: 650px) {
+      padding: 2rem;
+    }
+}
 </style>
+
+// {
+//    "entity_id": 1221,
+//    "sku": "FL000980",
+//    "is_cannabis": 1,
+//    "manufacturer": "Perfect",
+//    "name": "Happy Camper 1g ($1 Deal - Discount auto-applies at checkout)",
+//    "url": "/brands/perfect/happy-camper-1g/",
+//    "category": "Flower",
+//    "sub_category": "Pre-pack",
+//    "image_url": "FL000980/Perfect_FL_1gm_HappyCamper_PandP",
+//    "price": 18,
+//    "thc_percentage": 34.9,
+//    "quantity": 0
+//  },
+
+// Flower, Extract, Cartridge, Preroll, Edible, Pill
--- amuse-app/src/components/Product.vue
+++ amuse-app/src/components/Product.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="card">
     <h2>{{ name }}</h2>
-      <p>${{ price }}</p>
+    <!-- <img :src="imagePath"> -->
+    <p>${{ price }}</p>
+    <p>{{ category }}</p>
   </div>
 </template>
 
@@ -19,6 +21,11 @@ export default ({
     "price",
     "thc_percentage"
   ],
+  computed: {
+    imagePath: function() {
+      return `https://res.cloudinary.com/amusenow/w_auto,c_scale/${this.image_url}`;
+    }
+  }
 })
 </script>
 
@@ -26,7 +33,22 @@ export default ({
   .card {
     padding: 2rem;
     box-shadow: 0px 3px 6px #dddddd;
-    margin: 1rem;
+    // margin: 1rem;
     text-align: center;
   }
 </style>
+
+// {
+//    "entity_id": 1221,
+//    "sku": "FL000980",
+//    "is_cannabis": 1,
+//    "manufacturer": "Perfect",
+//    "name": "Happy Camper 1g ($1 Deal - Discount auto-applies at checkout)",
+//    "url": "/brands/perfect/happy-camper-1g/",
+//    "category": "Flower",
+//    "sub_category": "Pre-pack",
+//    "image_url": "FL000980/Perfect_FL_1gm_HappyCamper_PandP",
+//    "price": 18,
+//    "thc_percentage": 34.9,
+//    "quantity": 0
+//  },
