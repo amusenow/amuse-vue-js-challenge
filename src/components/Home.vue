edd8c5f5a0074b5432d7d37e11797ea1cd051874
Brent Richison <brentrichison@adt.com>
2021-12-08 01:52:24 -0600

    age modal

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,56 @@
 <template>
-  <Message severity="info" :closable="false">
-    <strong>Select your products</strong>
-  </Message>
+  <Dialog
+    header="Confirm Age"
+    :breakpoints="{ '960px': '75vw', '640px': '100vw' }"
+    :style="{ width: '50vw' }"
+    :modal="true"
+    :visible="true"
+  >
+    <div class="grid">
+      <div class="col-12">
+        <Message severity="info" :closable="false">
+          <strong>You’re 21+, right?</strong>
+        </Message>
+      </div>
+    </div>
+
+    <template #footer>
+      <div class="grid">
+        <div class="col-6 md:col-3">
+          <Button label="No" @click="exit" class="p-button-outlined" />
+        </div>
+
+        <div class="col-6 md:col-3">
+          <Button label="Yes" @click="enter" />
+        </div>
+      </div>
+    </template>
+  </Dialog>
 </template>
 
 <script>
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
 import Message from 'primevue/message';
+import router from '@/router';
 
 export default {
-  components: {
-    Message
+  components: { Button, Dialog, Message },
+
+  setup() {
+    const enter = () => router.push('/categories');
+    const exit = () => (window.location.href = 'http://www.google.com');
+    return { enter, exit };
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.p-dialog {
+  .p-dialog-footer {
+    button {
+      width: 100%;
+    }
+  }
+}
+</style>
